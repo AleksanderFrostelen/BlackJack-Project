cd3a5241d799c8332555f6cfa45409b1f8475bb5
moagardsater <moagardsater@192.168.20.168>
2017-10-04 16:01:46 +0200

    first draft

--- BlackJack/src/Dealer.java
+++ BlackJack/src/Dealer.java
@@ -1,4 +1,29 @@
-
+import java.util.ArrayList;
 public class Dealer {
+	
+	private final int stopValue = 17; //dealern stannar på 17 eller över.
+	
+	int totalValue; //Totala värdet för korten.
+	
+	ArrayList <Integer> hand = new ArrayList<Integer>();
+	
+	
+	
+	
+
+	public int getTotalValue() {
+		return totalValue;
+	}
+
+	public void setTotalValue(int totalValue) {
+		this.totalValue = totalValue;
+	}
+
+	public int getStopValue() {
+		return stopValue;
+	}
+	
+	
+
 
 }
