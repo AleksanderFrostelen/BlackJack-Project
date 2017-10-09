c5903232f26a39d0a5f506b772a2185564f4123a
steffodimfelt <steffodimfelt@192.168.0.104>
2017-10-09 23:25:48 +0200

    Splitting metoden är fixa

--- BlackJack/src/Betting.java
+++ BlackJack/src/Betting.java
@@ -1,27 +1,25 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class Betting {
 
-	public ArrayList <Integer> playerBet = new ArrayList<>();	
-	 
+	public ArrayList<Integer> playerBet = new ArrayList<>();
+
 	private int playerChips = 10;
 	private Scanner scan = new Scanner(System.in);
-	private int bettingValue=0;
+	private int bettingValue = 0;
 
-	
-	public ArrayList<Integer> getPlayerBet() {
-		return playerBet;
-	}
-
-	public void setPlayerBet(ArrayList<Integer> playerBet) {
-		this.playerBet = playerBet;
-		
-	}
+	// public ArrayList<Integer> getPlayerBet() {
+	// return playerBet;
+	// }
+	//
+	// public void setPlayerBet(ArrayList<Integer> playerBet) {
+	// this.playerBet = playerBet;
+	//
+	// }
 
 	public int getPlayerChips() {
-		
+
 		return playerChips;
 	}
 
@@ -37,135 +35,120 @@ public class Betting {
 		this.bettingValue = bettingValue;
 	}
 
+	public int bettingLoop() {
+		boolean bettingAgain = false;
+		int bettingIn = 0;
+		do {
+			if (getPlayerChips() > 0) {
+				System.out.println("Du har $" + getPlayerChips() + ". Hur mycket vill du satsa?");
 
+				bettingIn = bettingScanEvaluator();
 
-	/**Har man inga pengar kan man inte betta - skriv ut att man inte kan betta mer. 
-	 * har man pengar räknar man första handen playerBet[0]
-	 * finns det fler händer fortsätter man 
-	 * 
-	 * Om man har 7-11 kan man dubbla
-	 * Om man inte har pengar får man istället upp att man kan satsa sina sista slantar
-	 * Om man inte har några pengar kvar får man upp att man inte kan dubbla 
-	 * 
-	 * 
-	 * Om man satsat sin sista slant och förlorat så åker man ut. 
-	 * 
-	 * @param betIn
-	 * @param chipsIn
-	 */
-	
-	//vinst ger 1x pengar. Alltså 10 man satsade + 10
-	//Har man 21 och vinner får man 1.5x peng. Alltså 10 man satsade + 15
-	//Har dealer och playern lika mellan 17-21 får man tillbka instatsen.  
-	//Har spelaren 7-11 på grundgiven kan spelaren dubbla insatsen. 
-	//felmeddelande om man bettar med för mycket pengar. 
-	//är pengarna slut ska spelet sluta auto. ett felmeddelande att pengarna är slut. 
-	
-	  public int bettingLoop() 
-	  {
-		   boolean bettingAgain=false;
-		   int bettingIn=0;
-	   do{
-	     if (getPlayerChips()>0)
-	      {
-			  System.out.println("Du har $"+getPlayerChips()+". Hur mycket vill du satsa?");
-			  
-			  bettingIn = bettingScanEvaluator();
-
-			  if (bettingIn<=getPlayerChips())
-			  {
-				  setPlayerChips(getPlayerChips()-bettingIn); 
-				  setBettingValue(bettingIn);
-				  System.out.println("Du satsar $" + getBettingValue()+". Du har $"+getPlayerChips()+" kvar.");
-				  playerBet.add(getBettingValue());
-				  bettingAgain=false;
-			  }else {
-				  System.out.println("Du kan inte satsa så mycket.");
-				  bettingAgain=true;
-			  }
-			  
-	      }else{
-		      bettingAgain=false;
-		      System.out.println("Du har inga pengar kvar att satsa med.");
-	      }
-	     }while(bettingAgain==true);	   
-	   return bettingIn;
-	  
-	  }
-
-	  
-
-	  public void doubleUp(int bettingValue, int bettingIndex)
-	  {
-		  int doubleValue = getBettingValue()*2;
-		  boolean doubleUp = false;
-		  int tempChips = getPlayerChips();
-		  
-		  if (bettingValue >=7 && bettingValue<=11)
-		  {
-			  System.out.println(tempChips+" "+doubleValue);
-			  
-			  if (tempChips>=doubleValue)
-			  {
-				  System.out.println("Vill du dubbla insatsen? Ja eller Nej");
-				  String scannerAnswer = scan.next().toLowerCase();
-				  doubleUp = yesOrNo(scannerAnswer, "Vill du dubbla insatsen? Ja eller Nej");
-			 
-				if (doubleUp==true)
-				  {
-					  setPlayerChips(tempChips-doubleValue);
-					  playerBet.set(0,doubleValue);
-					  System.out.println("Du dubblade till $" + doubleValue+". Du har $"+getPlayerChips()+" kvar.");
-				  } 
-			  }
-			  
-			  if(tempChips<doubleValue&&getPlayerChips()>0)
-			  {
-				  System.out.println("Din hand har summan $"+bettingValue+". Du kan inte dubbla insatsen, \nmen du kan satsa återstående pengar. \nVill du det? Ja eller Nej");
-				  String scannerAnswer = scan.next().toLowerCase();
-				  doubleUp = yesOrNo(scannerAnswer, "Din hand har summan $"+bettingValue+". Du kan inte dubbla insatsen, \nmen du kan satsa återstående pengar $"+getPlayerChips()+". \nVill du det? Ja eller Nej");
-					if (doubleUp==true)
-					  {
-						  playerBet.set(0,playerBet.get(0)+getPlayerChips());
-						  setPlayerChips(0);
-						  System.out.println("Du ökade till $" + playerBet.get(0)+". Du har $"+getPlayerChips()+" kvar.");
-					  } 
-			  }
-		  }
-		  
-
-		  
-	  }
-	  
-	  int bettingScanEvaluator ()
-	  {
-		  int bettingIn;
-		  do {
-		      while (!scan.hasNextInt()) {
-		    	  	System.out.println("Satsa gärna ha ett rikigt belopp. Försök igen.");
-		    	  	scan.next(); 
-		      }
-		      bettingIn = scan.nextInt();
-		  } while (bettingIn <= 0);
-		  return bettingIn;
-	  }
-	  
-	  //GÖR OM DET BLIR FEL I DEFAULT
-		boolean yesOrNo(String answerIn, String printOut)
-		{
-			boolean returnAnswer=true;
-			boolean fetchReturnAnswer=false;
-			do {
-				switch (answerIn)
-				{
-					case "j": case "ja": returnAnswer=true;fetchReturnAnswer=true;break;
-					case "n": case "nej": returnAnswer=false;fetchReturnAnswer=true;break;
-					default: System.out.println(printOut);fetchReturnAnswer=false;break;
+				if (bettingIn <= getPlayerChips()) {
+					setPlayerChips(getPlayerChips() - bettingIn);
+					setBettingValue(bettingIn);
+					System.out.println("Du satsar $" + getBettingValue() + ". Du har $" + getPlayerChips() + " kvar.");
+					playerBet.add(getBettingValue());
+					bettingAgain = false;
+				} else {
+					System.out.println("Du kan inte satsa så mycket.");
+					bettingAgain = true;
 				}
-			} while (fetchReturnAnswer==false);
-			return returnAnswer;
+				
+			} else {
+				bettingAgain = false;
+				System.out.println("Du har inga pengar kvar att satsa med.");
+			}
+		} while (bettingAgain == true);
+		return bettingIn;
+	}
+
+	public void doubleUp(int bettingValue, int bettingIndex) {
+		int doubleValue = getBettingValue() * 2;
+		boolean doubleUp = false;
+		int tempChips = getPlayerChips();
+
+		if (bettingValue >= 7 && bettingValue <= 11) {
+			
+			if (tempChips >= doubleValue) {
+				System.out.println("Vill du dubbla insatsen? Ja eller Nej");
+				String scannerAnswer = scan.next().toLowerCase();
+				doubleUp = yesOrNo(scannerAnswer, "Vill du dubbla insatsen? Ja eller Nej");
+
+				if (doubleUp == true) {
+					setPlayerChips(tempChips - doubleValue);
+					playerBet.set(0, doubleValue);
+					System.out.println("Du dubblade till $" + doubleValue + ". Du har $" + getPlayerChips() + " kvar.");
+				}
+			}
+
+			if (tempChips < doubleValue && getPlayerChips() > 0) {
+				System.out.println("Din hand har summan $" + bettingValue
+						+ ". Du kan inte dubbla insatsen, \nmen du kan satsa återstående pengar. \nVill du det? Ja eller Nej");
+				String scannerAnswer = scan.next().toLowerCase();
+				doubleUp = yesOrNo("Din hand har summan $" + bettingValue + ". Du kan inte dubbla insatsen, \nmen du kan satsa återstående pengar $" + getPlayerChips() + ". \nVill du det? Ja eller Nej");
+				if (doubleUp == true) {
+					playerBet.set(0, playerBet.get(0) + getPlayerChips());
+					setPlayerChips(0);
+					System.out
+							.println("Du ökade till $" + playerBet.get(0) + ". Du har $" + getPlayerChips() + " kvar.");
+				}
+			}
 		}
+	}
 
+	int bettingScanEvaluator() {
+		int bettingIn;
+		do {
+			while (!scan.hasNextInt()) {
+				System.out.println("Satsa gärna ha ett rikigt belopp. Försök igen.");
+				scan.next();
+			}
+			bettingIn = scan.nextInt();
+		} while (bettingIn <= 0);
+		return bettingIn;
+	}
 
-	  
+	void bettingPayBack(int stakeIn, double multipleIn) {
+		double newValue = (stakeIn * multipleIn) + stakeIn;
+		newValue = newValue + getPlayerChips();
+		int newValueOut = (int) Math.round(newValue);
+		setPlayerChips(newValueOut);
+	}
+
+	void onlyStakePayBack(int stakeIn) {
+		double newValue = stakeIn + getPlayerChips();
+		int newValueOut = (int) Math.round(newValue);
+		setPlayerChips(newValueOut);
+	}
+
+	// GÖR OM DET BLIR FEL I DEFAULT
+	boolean yesOrNo(String printOut) {
+		boolean returnAnswer = true;
+		boolean fetchReturnAnswer = false;
+
+		do {
+			System.out.println(printOut);
+			String scannerAnswer = scan.next().toLowerCase();
+			
+			switch (scannerAnswer) {
+			case "j":
+			case "ja":
+				returnAnswer = true;
+				fetchReturnAnswer = true;
+				break;
+			case "n":
+			case "nej":
+				returnAnswer = false;
+				fetchReturnAnswer = true;
+				break;
+			default:
+				System.out.println(printOut);
+				fetchReturnAnswer = false;
+				break;
+			}
+		} while (fetchReturnAnswer == false);
+		return returnAnswer;
+	}
+
 }
--- BlackJack/src/Deck.java
+++ BlackJack/src/Deck.java
@@ -1,92 +1,86 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.Arrays;
 
 public class Deck {
-	
-	private final int[] VALUESPAN = {2,3,4,5,6,7,8,9,10,20,30,40,11};
-	private int[] deckSetup = new int [52];
+
+	private final int[] VALUESPAN = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 11 };
+	private int[] deckSetup = new int[52];
 	private LinkedList<Integer> shoe = new LinkedList<>();
-	
+
 	public int[] getDeckSetup() {
 		return deckSetup;
 	}
+
 	public void setDeckSetup(int[] deckSetup) {
 		this.deckSetup = deckSetup;
 	}
+
 	public LinkedList<Integer> getShoe() {
 		return shoe;
 	}
+
 	public void setShoe(LinkedList<Integer> shoe) {
 		this.shoe = shoe;
 	}
 
-	
-	//Lägger upp alla kort i grundleken deckSetup
-	void deckSetup()
-	{
-		for (int VALUESPANElem=0;VALUESPANElem<deckSetup.length;VALUESPANElem=VALUESPANElem+VALUESPAN.length)//Loopar igenom hela array deckSpan med repeat VALUESPAN.length
-		{
-			System.arraycopy(VALUESPAN, 0, deckSetup, VALUESPANElem, VALUESPAN.length);//Kopierar hela array VALUESPAN till array deckSpan
+	// Lägger upp alla kort i grundleken deckSetup
+	void deckSetup() {
+		for (int VALUESPANElem = 0; VALUESPANElem < deckSetup.length; VALUESPANElem = VALUESPANElem + VALUESPAN.length)// Loopar igenom hela array deckSpan med repeat VALUESPAN.length
+		{System.arraycopy(VALUESPAN, 0, deckSetup, VALUESPANElem, VALUESPAN.length);}// Kopierar hela array VALUESPAN till array deckSpan
+	}
+
+	// Blandar en ny lek att spela med.
+	void shuffle() {
+		for (int LinkedElem = 0; LinkedElem < deckSetup.length; LinkedElem++) {
+			shoe.add(deckSetup[LinkedElem]);// Lägger in varje element från array deckSetup till LinkedList shoe
 		}
 	}
-	
-	//Blandar en ny lek att spela med.
-	void shuffle()
-	{	
-		for (int LinkedElem=0;LinkedElem<deckSetup.length;LinkedElem++)
-		{
-			shoe.add(deckSetup[LinkedElem]);//Lägger in varje element från array deckSetup till LinkedList shoe
-		}
-		
-	}
-	
-	//Returnerar ett slumpmässigt nummer
-	int randomCard (int randomIn)
-	{
-		int randomOut = (int)(Math.random()*randomIn);
+
+	// Returnerar ett slumpmässigt nummer
+	int randomCard(int randomIn) {
+		int randomOut = (int) (Math.random() * randomIn);
 		return randomOut;
 	}
-	
-	//Skriver ut värdet i LinkedList shoe från inkommande attribut
-	int shoeValue(int cardNmbIn)
-	{
-		//System.out.println(shoe.get(cardNmbIn).toString());
-		return shoe.get(cardNmbIn);
-	}
-	
-	public void resetAll(Player playerObj,Player dealerObj) //shoe ska laddas om till deckSetup och båda händerna ska nollas.
+
+	// Skriver ut värdet i LinkedList shoe från inkommande attribut
+	int shoeValue(int cardNmbIn) {return shoe.get(cardNmbIn);}
+
+	public void resetAll(Player playerObj, Player dealerObj) // shoe ska laddas om till deckSetup och båda händerna ska nollas.
 	{
 		playerObj.hand.clear();
 		playerObj.hand.clear();
 		shoe.clear();
-		playerObj.hand.add(new ArrayList<Integer>());//Adderar ett element till player
-		dealerObj.hand.add(new ArrayList<Integer>());//Adderar ett element till dealer
+		playerObj.hand.add(new ArrayList<Integer>());// Adderar första raden till player
+		dealerObj.hand.add(new ArrayList<Integer>());// Adderar första raden till dealer
 		shuffle();
 	}
-	
-	public void dealRandomCards() //den ska använda randomCard och ger kort till player och dealer
+
+	public void dealRandomCards() // den ska använda randomCard och ger kort till player och dealer
 	{
 
 	}
-	
-	public void removeCard(Object object) //den ska använda randomCard och ger kort till player och dealer
+
+	public void removeCard(Object object) // den ska använda randomCard och ger kort till player och dealer
 	{
 
 	}
-	
-	String showAllCards(Player playerObj,int handIndex)//Returnerar en sträng med valörerna på alla korten på vald hand. 
+
+	String showAllCards(Player playerObj, int handIndex)// Returnerar en sträng med valörerna på alla korten på vald
+														// hand.
 	{
-		String tempString="";
-		for(int handElem=0;handElem<playerObj.hand.get(handIndex).size();handElem++)
-		{tempString += playerObj.hand.get(handIndex).get(handElem).toString()+" ";}
+		String tempString = "";
+		for (int handElem = 0; handElem < playerObj.hand.get(handIndex).size(); handElem++) {
+			tempString += playerObj.hand.get(handIndex).get(handElem).toString() + " ";
+		}
 		return tempString;
 	}
-	
-	int showFirstCard(Player playerObj,int handIndex, int cardIndex)//Returnerar valören på det första kortet på hand.
-	{return playerObj.hand.get(handIndex).get(cardIndex);}
-	
-	int totalHandValue(Player playerObj,int handIndex)//Returnerar totalen för en hand.
+
+	int showOneCard(Player playerObj, int handIndex, int cardIndex)// Returnerar valören på det första kortet på hand.
+	{
+		return playerObj.hand.get(handIndex).get(cardIndex);
+	}
+
+	int totalHandValue(Player playerObj, int handIndex)// Returnerar totalen för en hand.
 	{
 		int handTotal = playerObj.hand.get(handIndex).stream().mapToInt(Integer::intValue).sum();
 		return handTotal;
