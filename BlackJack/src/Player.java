c4ea366e98751e5879e48f702aef116d33eacc9f
steffodimfelt <steffodimfelt@192.168.20.128>
2017-10-13 11:29:15 +0200

    version 3.1

--- BlackJack/src/Player.java
+++ BlackJack/src/Player.java
@@ -3,14 +3,12 @@ import java.util.ArrayList;
 public class Player {
 
 	private final int STOPVALUE = 17; // dealern stannar på 17 eller över.
-	private int totalValue; // Totala värdet för korten.
 	protected ArrayList<ArrayList<Integer>>hand;
 	protected ArrayList<ArrayList<String>> handCol;
 
 
 	public Player() {
 		super();
-		totalValue=0;
 		hand = new ArrayList<>();
 		handCol = new ArrayList<>();
 	}
@@ -19,12 +17,5 @@ public class Player {
 		return STOPVALUE;
 	}
 
-	public int getTotalValue() {
-		return totalValue;
-	}
-
-	public void setTotalValue(int totalValue) {
-		this.totalValue = totalValue;
-	}
 	
 }
